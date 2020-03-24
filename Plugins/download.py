a1e681abb6e4d6ad7c3675ba02d359a239dfa382
M.Furkan <byazade@yandex.com>
2020-03-24 12:30:13 +0300

    edit

--- Plugins/download.py
+++ Plugins/download.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, Filters
-from os import listdir, remove, stat
+from os import listdir, remove, stat, makedirs
 from time import sleep, time
 
 
@@ -19,23 +19,25 @@ def download(app, message):
     if message.reply_to_message:
         message.edit('Downloading...')
         start = time()
+        if not os.path.isdir("./DOWNLOADS/"):
+            os.makedirs("./DOWNLOADS/")
         if message.reply_to_message.document:
             if len(message.text.split()) == 1:
                 file_name = message.reply_to_message.document.file_name
-                if file_name in listdir("./Downloads/"):
+                if file_name in listdir("./DOWNLOADS/"):
                     message.edit('There is already a file with this name')
                     return
-                app.download_media(message.reply_to_message, "./Downloads/{}".format(file_name))
+                app.download_media(message.reply_to_message, "./DOWNLOADS/{}".format(file_name))
                 end = time()
                 elapsed_time = str(end - start)
                 message.edit('File is downloaded\nElapsed time : {}'.format(elapsed_time))
                 return
             else:
                 file_name = " ".join(message.text.split()[1:]) + ".txt"
-                if file_name in listdir("./Downloads/"):
+                if file_name in listdir("./DOWNLOADS/"):
                     message.edit('There is already a file with this name')
                     return
-                app.download_media(message.reply_to_message, file_name="./Downloads/{}".format(file_name))
+                app.download_media(message.reply_to_message, file_name="./DOWNLOADS/{}".format(file_name))
                 end = time()
                 elapsed_time = str(end - start)
                 message.edit('File is downloaded\nFile name : {}\nElapsed time : {}'.format(file_name,
@@ -46,11 +48,11 @@ def download(app, message):
                 num = 0
                 file_name = "video{}.mp4"
                 while 1:
-                    if file_name.format(num) in listdir("./Downloads/"):
+                    if file_name.format(num) in listdir("./DOWNLOADS/"):
                         num += 1
                     else:
                         break
-                app.download_media(message.reply_to_message, file_name="./Downloads/{}".format(file_name.format(num)))
+                app.download_media(message.reply_to_message, file_name="./DOWNLOADS/{}".format(file_name.format(num)))
                 end = time()
                 elapsed_time = str(end - start)
                 message.edit('Video is downloaded\nFile name : {}\nElapsed time : {}'.format(file_name,
@@ -58,7 +60,7 @@ def download(app, message):
                 return
             else:
                 file_name = " ".join(message.text.split()[1:]) + ".mp4"
-                if file_name in listdir("./Downloads/"):
+                if file_name in listdir("./DOWNLOADS/"):
                     message.edit('There is already a video with this name')
                     return
                 app.download_media(message.reply_to_message, file_name="./Downloads/{}".format(file_name))
@@ -72,11 +74,11 @@ def download(app, message):
                 num = 0
                 file_name = message.reply_to_message.audio.file_name
                 while 1:
-                    if file_name.format(num) in listdir("./Downloads/"):
+                    if file_name.format(num) in listdir("./DOWNLOADS/"):
                         num += 1
                     else:
                         break
-                app.download_media(message.reply_to_message, file_name="./Downloads/{}".format(file_name.format(num)))
+                app.download_media(message.reply_to_message, file_name="./DOWNLOADS/{}".format(file_name.format(num)))
                 end = time()
                 elapsed_time = str(end - start)
                 message.edit('Music is downloaded\nFile name : {}\nElapsed time : {}'.format(file_name,
@@ -84,10 +86,10 @@ def download(app, message):
                 return
             else:
                 file_name = " ".join(message.text.split()[1:]) + ".mp3"
-                if file_name in listdir("./Downloads/"):
+                if file_name in listdir("./DOWNLOADS/"):
                     message.edit('There is already a music with this name')
                     return
-                app.download_media(message.reply_to_message, file_name="./Downloads/{}".format(file_name))
+                app.download_media(message.reply_to_message, file_name="./DOWNLOADS/{}".format(file_name))
                 end = time()
                 elapsed_time = str(end - start)
                 message.edit('Music is downloaded\nFile name : {}\nElapsed time : {}'.format(file_name,
@@ -101,10 +103,10 @@ def download(app, message):
 def upload(app, message):
     message.edit('Wait...')
     file_name = " ".join(message.text.split()[1:])
-    if file_name in listdir("./Downloads/"):
+    if file_name in listdir("./DOWNLOADS/"):
         message.delete()
         start = time()
-        app.send_document(message.chat.id, "./Downloads/{}".format(file_name))
+        app.send_document(message.chat.id, "./DOWNLOADS/{}".format(file_name))
         elapsed = str(time() - start)
         app.send_message(message.chat.id, f"Elapsed time : {elapsed}")
 
@@ -116,9 +118,9 @@ def remove_file(app, message):
         num = 1
         file = message.text.split()[1]
         if file.isdigit():
-            for i in listdir("./Downloads/"):
+            for i in listdir("./DOWNLOADS/"):
                 if num == int(file):
-                    remove("./Downloads/{}".format(i))
+                    remove("./DOWNLOADS/{}".format(i))
                     message.edit('Removed successfully')
                     return
                 else:
@@ -131,8 +133,8 @@ def download_list(app, message):
     message.edit('Wait...')
     downloads = ""
     num = 1
-    for i in listdir("./Downloads/"):
-        downloads += f"📂 [{num}] {render_file_name(i)}            -      {int(stat('./Downloads/' + i).st_size / (1024 * 1024))} MB\n"
+    for i in listdir("./DOWNLOADS/"):
+        downloads += f"📂 [{num}] {render_file_name(i)}            -      {int(stat('./DOWNLOADS/' + i).st_size / (1024 * 1024))} MB\n"
         num += 1
     message.edit("Download list:\n\n<u>            File names            </u>        <u>      Size      .</u>\n"
                  + downloads, parse_mode='HTML') if num != 1 else message.edit('You has not a file')
